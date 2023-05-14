e3bb3368144231f70257a599f0d3b8ac8a62f29d
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2023-05-14 12:11:56 +0100

    first commit

---
+++ src/main/java/com/wedocode/jenkinsdevopsspring/JenkinsDevopsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wedocode.jenkinsdevopsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDevopsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDevopsSpringApplication.class, args);
+	}
+
+}
